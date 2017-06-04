17975f5c20eab0866be8bc357e09a6b103dcc091
Tredis <t.redis@gmail.com>
2017-06-04 17:16:56 -0400

    1) List is resizing on rotation; 2) Added list keys; 3) Made details full screen and fixed stats getting cut off; 4) Added code for retaining scroll position, but need to figure a way around RN orientation bug

--- app/native/containers/App.js
+++ app/native/containers/App.js
@@ -3,56 +3,84 @@ import { StyleSheet, Text, View, TextInput, Image, FlatList, TouchableHighlight 
 import {styles} from '../style/'
 import {getPhotos, select} from '../../redux/dispatchers/'
 import {connect} from 'react-redux';
+import Dimensions from 'Dimensions'
 
 class App extends Component {
   render() {
     console.log(this.props.photos)
     if(this.props.photos.selected == null){
-      return <View style={styles.container}>
-        <Image
-          style={{width: 200, height: 85}}
-          source={{uri: 'https://www.wpclipart.com/education/encouraging_words/Awesome.png'}}
-        />
-        <Text style={styles.welcome}>
-          Tell me what to search for
-        </Text>
-        <TextInput
-          style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}}
-          onSubmitEditing={ e => this.props.getPhotos(e.nativeEvent.text)}
-        />
-        <FlatList
-          data={this.props.photos.list}
-          renderItem={(photo) => (
-            <TouchableHighlight onPress={() => this.props.select(photo.index)}>
-              <Image
-                style={{width: 200, height: 200}}
-                source={{uri: photo.item.webformatURL}} />
-            </TouchableHighlight>
-          )}
-        />
-      </View>
-    }else{
-      let selected = this.props.photos.list[this.props.photos.selected]
       return (
         <View style={styles.container}>
+          <Image
+            style={{width: 200, height: 85}}
+            source={{uri: 'https://www.wpclipart.com/education/encouraging_words/Awesome.png'}}
+          />
           <Text style={styles.welcome}>
-            {"User: "+ selected.user +
-            ", Tags: "+selected.tags + 
-            ", Size: "+selected.webformatWidth+"x"+ selected.webformatHeight}
+            Tell me what to search for
           </Text>
-          <TouchableHighlight onPress={() => this.props.select(null)}>
-            <Image
-              style={{width: 400, height: 400}}
-              source={{uri: selected.webformatURL}} />
-          </TouchableHighlight>
+          <TextInput
+            style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}}
+            onSubmitEditing={ e => this.props.getPhotos(e.nativeEvent.text)}
+          />
+          <FlatList
+            data={this.props.photos.list}
+            keyExtractor={photo => photo.index}
+            ref={(ref) => { this.listRef = ref; }}
+            renderItem={(photo) => {
+              let width = Dimensions.get('window').width
+              return (
+                <TouchableHighlight onPress={() => this.props.select(photo.index)}>
+                  <Image
+                    style={{width: width, height: width * photo.item.webformatHeight / photo.item.webformatWidth}}
+                    source={{uri: photo.item.webformatURL}} />
+                </TouchableHighlight>
+              )
+            }}
+          />
         </View>
       )
+    }else{
+      return(
+        <View style={styles.container}>
+          <FlatList
+            data={[{data:this.props.photos.list[this.props.photos.selected], key: 1}]}
+            renderItem={({item}) => {
+              let {width, height} = Dimensions.get('window')
+              let w = item.data.webformatWidth
+              let h = item.data.webformatHeight
+              return (
+                <View style={styles.container}>
+                  <Text style={styles.welcome}>
+                    {"User: " + item.data.user +
+                    ", Tags: "+ item.data.tags +
+                    ", Size: "+ item.data.webformatWidth+"x"+item.data.webformatHeight}
+                  </Text>
+                  <TouchableHighlight onPress={() => this.props.select(null)}>
+                    <Image
+                      style={{width: (w*height > h*width ? width : height*w/h),
+                              height:(w*height > h*width ? width*h/w : height)}}
+                      source={{uri: item.data.webformatURL}} />
+                  </TouchableHighlight>
+                </View>
+              )
+            }}
+          />
+        </View>
+      )
+    }
+  }
+  componentDidUpdate(){
+    if(this.props.photos.scroll && !this.props.photos.selected){
+      this.listRef.scrollTop = this.listRef.scrollHeight/2; //  neither of these work
+      this.listRef.scrollToOffset({animated: true, offset: 100}) // for some reason
     }
   }
 }
 
 export default connect(st=>st, {getPhotos, select})(App);
 
+//onEndReached?
+//scrollToIndex(params: object)
 
 // {
 //     "total": 4692,
--- app/redux/reducers/photos.js
+++ app/redux/reducers/photos.js
@@ -2,19 +2,21 @@ import { GET_PHOTOS, ADD_PHOTOS, SELECT } from '../constants/';
 
 const initialState = {
   selected: null,
+  scroll: 0,
   list: []
 }
 export default function reducer (photos = initialState, action) {
   switch (action.type) {
     case GET_PHOTOS:{
-      return ({selected: null, list: action.list})
+      return {selected: null, list: action.list, scroll: 0}
     }
     case ADD_PHOTOS: {
-      return ({selected: null, list: concat(photos.list, action.list)})
+      return {selected: null, list: concat(photos.list, action.list), scroll: 0}
     }
     case SELECT: {
-      console.log
-      return Object.assign({}, photos, {selected: action.i})
+      return Object.assign({}, photos, action.i == null ? 
+                                        {selected: null} 
+                                       :{selected: action.i, scroll: action.i})
     }
     default: return photos;
   }
